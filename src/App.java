22543f79519af287ec5e613a79e904f2072f1425
Matheus <matheusalmeida016@gmail.com>
2023-06-23 22:30:37 -0400

    feat: test references

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Account client = new Account();
+        Account newClient = client;
 
         client.accounHolder = "Matheus Almeida";
         client.accountBalance = 200;
@@ -8,7 +9,12 @@ public class App {
         System.out.println(client.accountBalance);
         
         client.accountBalance += 300;
-        
+
         System.out.println(client.accountBalance);
+
+        newClient.accountBalance = 0;
+
+        System.out.println("client account balance is " + client.accountBalance);
+        System.out.println("newClient account balance is " + newClient.accountBalance);
     }
 }
