caa053c6d7b8d4d0bf07dc78288d34770e608722
Matheus <matheusalmeida016@gmail.com>
2023-06-30 23:32:43 -0400

    feat: add manager  extending employee

--- src/App.java
+++ src/App.java
@@ -1,8 +1,14 @@
 public class App {
     public static void main(String[] args) {
         Employee empployee = new Employee("Tyler", 1112223331, 3000);
+        Manager manager = new Manager("Bob", 1112223334, 7000, "1234");
 
         System.out.println(empployee.getName());
         System.out.println(empployee.getBonification());
+
+        System.out.println(manager.getName());
+        System.out.println(manager.getBonification());
+        System.out.println(manager.authentication("5768"));
+        System.out.println(manager.authentication("1234"));
     }
 }
---
+++ src/Manager.java
@@ -0,0 +1,20 @@
+public class Manager extends Employee {
+  private String password;
+
+  public Manager(String name, int id, double compensation, String password) {
+    super(name, id, compensation);
+    this.password = password;
+  }
+
+  public boolean authentication(String password) {
+    if (password == this.password) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public double getBonification() {
+    return this.getCompensation() * 0.8;
+  }
+}
