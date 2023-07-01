0550df0512c8a1b03a651a39cf4ce0faaa9515f8
Matheus <matheusalmeida016@gmail.com>
2023-06-30 21:37:37 -0400

    feat: add employee class

---
+++ src/Employee.java
@@ -0,0 +1,24 @@
+public class Employee {
+  private String name;
+  private int id;
+  private double compensation;
+
+  public Employee(String name, int id, double compensation) {
+    this.name = name;
+    this.id = id;
+    this.compensation = compensation;
+  }
+
+  // getters
+  public String getName() {
+    return this.name;
+  }
+
+  public int getId() {
+    return this.id;
+  }
+
+  public double getCompensation() {
+    return this.compensation;
+  }
+}
