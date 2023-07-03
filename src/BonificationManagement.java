7e8639e7b5764629fbc29e6274dd13c128f89a8b
Matheus <matheusalmeida016@gmail.com>
2023-07-03 11:11:27 -0400

    feat: add bonification management class and use polymorphism

---
+++ src/BonificationManagement.java
@@ -0,0 +1,13 @@
+public class BonificationManagement {
+  private double total;
+
+  // getters
+  public double getTotal() {
+    return this.total;
+  }
+
+  // setters 
+  public void registerEmployee(Employee employee) {
+    this.total += employee.getBonification();
+  }
+}
