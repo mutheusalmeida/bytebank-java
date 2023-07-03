127085216fb11a8da747f82e652a9652546ad163
Matheus <matheusalmeida016@gmail.com>
2023-07-03 12:09:33 -0400

    feat: make use of inheritance with extends

--- src/App.java
+++ src/App.java
@@ -2,21 +2,5 @@ public class App {
     public static void main(String[] args) {
         Employee employee = new Employee("Tyler", 1112223331, 3000);
         Manager manager = new Manager("Bob", 1112223334, 7000, "1234");
-        BonificationManagement bonificationManagement = new BonificationManagement();
-
-        System.out.println(employee.getName());
-        System.out.println(employee.getBonification());
-        System.out.println(employee.getCompensation());
-
-        System.out.println(manager.getName());
-        System.out.println(manager.getBonification());
-        System.out.println(manager.getCompensation());
-        System.out.println(manager.authentication("5768"));
-        System.out.println(manager.authentication("1234"));
-
-        bonificationManagement.registerEmployee(employee);
-        bonificationManagement.registerEmployee(manager);
-
-        System.out.println(bonificationManagement.getTotal());
     }
 }
---
+++ src/SavingAccount.java
@@ -0,0 +1,5 @@
+public class SavingAccount extends Account {
+  public SavingAccount(Client client, int branch, int checkDigit, double accountBalance) {
+    super(client, branch, checkDigit, accountBalance);
+  }
+}
