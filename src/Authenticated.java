c2b4233be91bd6b9590fe2f3cfb3199fe37252cd
Matheus <matheusalmeida016@gmail.com>
2023-07-04 10:54:18 -0400

    improve: remove final interface attribute

--- src/Authenticated.java
+++ src/Authenticated.java
@@ -1,6 +1,4 @@
 public abstract interface Authenticated {
-  public String password = "";
-
   public abstract void setPassword(String password);
 
   public abstract String authenticate(String password);
