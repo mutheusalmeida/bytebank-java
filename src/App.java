836011ee9b3a388debdbb4d41824bcc43c3d5dd2
Matheus <matheusalmeida016@gmail.com>
2023-07-06 12:50:51 -0400

    feat: handle error

--- src/App.java
+++ src/App.java
@@ -2,8 +2,12 @@ public class App {
     public static void main(String[] args) {
         CheckingAccount client = new CheckingAccount(new Client(), 123, 4, 4000);
         TaxCalculator taxCalculator = new TaxCalculator();
-
-        taxCalculator.registerTaxable(client);
+        
+        try {
+          taxCalculator.registerTaxable(client);
+        } catch(ArithmeticException ex) {
+          System.err.println(ex.getMessage());
+        }
 
         System.out.println(taxCalculator.getTotalTax());
     }
