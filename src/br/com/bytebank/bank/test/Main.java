a066405499ef357128c63cd86e48623ec4fcadae
Matheus Almeida <matheusalmeida016@gmail.com>
2023-07-20 18:26:01 -0400

    feat: compare if array list contains object with the same values

--- src/br/com/bytebank/bank/test/Main.java
+++ src/br/com/bytebank/bank/test/Main.java
@@ -7,10 +7,18 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         Account acc = new CheckingAccount(new Client("930.040.303-14"), 123, 4, 4000);
-        Account acc2 = new SavingAccount(new Client("120.026.194-04"), 123, 4, 2000);
+        Account acc2 = new SavingAccount(new Client("120.026.194-04"), 122, 7, 2000);
 
-        boolean isEqual = acc.equals(acc2);
+        ArrayList<Account> accountsStorage = new ArrayList<>();
 
-        System.out.println(isEqual);
+        accountsStorage.add(acc);
+
+        boolean contains = accountsStorage.contains(acc2);
+
+       if (!contains) {
+           accountsStorage.add(acc2);
+       }
+
+       System.out.println(accountsStorage.size());
     }
 }
