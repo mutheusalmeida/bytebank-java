552f65a05c42af08da637b0a19396824e1888796
Matheus Almeida <matheusalmeida016@gmail.com>
2023-08-02 13:16:39 -0400

    feat: use vector list

--- src/br/com/bytebank/bank/test/Main.java
+++ src/br/com/bytebank/bank/test/Main.java
@@ -3,13 +3,15 @@ package br.com.bytebank.bank.test;
 import br.com.bytebank.bank.model.*;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
 
 public class Main {
     public static void main(String[] args) {
         Account acc = new CheckingAccount(new Client("930.040.303-14"), 123, 4, 4000);
         Account acc2 = new SavingAccount(new Client("120.026.194-04"), 122, 7, 2000);
 
-        ArrayList<Account> accountsStorage = new ArrayList<>();
+        List<Account> accountsStorage = new Vector<>();
 
         accountsStorage.add(acc);
 
